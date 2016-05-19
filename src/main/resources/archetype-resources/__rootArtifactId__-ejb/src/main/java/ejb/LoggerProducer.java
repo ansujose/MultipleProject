55e270b5a60676c18eea63ae409ae96935b2586c
Jose <Ansu_Jose@AMER.DELL.COM>
2016-05-19 16:18:45 +0530

    commit

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-ejb/src/main/java/ejb/LoggerProducer.java
@@ -0,0 +1,24 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.ejb;
+
+import javax.enterprise.inject.Produces;
+import javax.enterprise.inject.spi.InjectionPoint;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LoggerProducer {
+
+    @Produces
+    public Logger getLogger(InjectionPoint injectionPoint) {
+        Class<?> clazz;
+        if (injectionPoint.getBean() != null) {
+            clazz = injectionPoint.getBean().getBeanClass();
+        } else {
+            clazz = injectionPoint.getMember().getDeclaringClass();
+        }
+        return LoggerFactory.getLogger(clazz);
+    }
+}
